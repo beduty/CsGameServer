1f00455301693a7c75d338a490807a2824632880
jungty6735 <80371391+HUTOM-junty6735@users.noreply.github.com>
2021-07-25 00:23:39 +0900

    Async Accept

---
+++ CsGameServer/ServerCore/Listener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        
+
+        public void Init(IPEndPoint endPoint)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _listenSocket.Bind(endPoint);
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            // 당장 완료되지 않음.
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+            {
+                OnAcceptCompleted(null, args);
+            }
+        }
+
+        void OnAcceptCompleted(Object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                // Todo
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+            RegisterAccept(args);
+        }
+
+        public Socket Accept()
+        {
+            // blocking 
+            // _listenSocket.Accept();
+
+
+            return
+        }
+    }
+}
--- CsGameServer/ServerCore/Program.cs
+++ CsGameServer/ServerCore/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
     class Program
     {
+        static Listener _listener =  new Listener();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress iPAddress = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, 7777);
+
+            _listener.Init(endPoint);
+
+            while (true)
+            {
+                Console.WriteLine("Listening...");
+
+                // accept 
+                Socket clientSocket = _listener.Accept();
+
+                // receive
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // send
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcom to MMORPG Server !");
+                clientSocket.Send(sendBuff);
+                
+                // close
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
         }
     }
 }
